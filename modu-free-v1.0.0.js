04bb1b3121169dd30c76b9694a3c20d5211406f5
Goutam <60634430+goutamji810@users.noreply.github.com>
2022-01-01 15:26:18 +0530

    Update modu-free-v1.0.0.js

--- modu-free-v1.0.0.js
+++ modu-free-v1.0.0.js
@@ -1,1 +1,16 @@
 
+//create by goutam
+//https://cdn.jsdelivr.net/gh/{username}/{repo}/{file name}
+//btn id = download_btn-oeneksbdosndofnmemend
+//ads container = ads-container-modu-sjsowjsishsjshajajbshdkensodnw
+var id="download_btn-oeneksbdosndofnmemend";
+
+
+window.onload = function() {
+  var div=document.getElementsByClassName(id)[0];
+var d='<a href="https://youtube.com/channel/UCpAdTb1N1KiKoTi19zOh0ZA" target="_blank"><img src="https://i.ibb.co/Qdnw1N8/Happy-New-Year-wishes-min.jpg" alt="Happy-New-Year-wishes-min" height="200" width="auto" border="0"></a>';  
+div.style.textAlign="center";
+div.innerHTML=d;
+
+
+};
