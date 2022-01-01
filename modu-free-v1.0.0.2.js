808e0380e790fa88bb77a47f13205e65fd85f9d0
Goutam <60634430+goutamji810@users.noreply.github.com>
2022-01-01 15:50:42 +0530

    Update and rename modu-free-v1.0.0.1.js to modu-free-v1.0.0.2.js

--- modu-free-v1.0.0.2.js
+++ modu-free-v1.0.0.2.js
@@ -7,9 +7,9 @@ var id="download_btn-oeneksbdosndofnmemend";
 
 window.onload = function() {
   var div=document.getElementsByClassName(id)[0];
-var d='<div><a href="https://youtube.com/channel/UCpAdTb1N1KiKoTi19zOh0ZA" target="_blank"><img src="https://i.ibb.co/Qdnw1N8/Happy-New-Year-wishes-min.jpg" alt="Happy-New-Year-wishes-min" height="200" width="auto" border="0"></a></div>';  
+var d='<br><br><div><a href="https://youtube.com/channel/UCpAdTb1N1KiKoTi19zOh0ZA" target="_blank"><img src="https://i.ibb.co/Qdnw1N8/Happy-New-Year-wishes-min.jpg" alt="Happy-New-Year-wishes-min" height="200" width="auto" border="0"></a></div>';  
 div.style.textAlign="center";
-div.innerHTML=d;
+div.innerHTML+=d;
 
 
 };
