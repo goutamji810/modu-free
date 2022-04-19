21dc9f0a1e99871b821b141510243dd00056ef1b
Goutam <60634430+goutamji810@users.noreply.github.com>
2022-04-19 13:43:57 +0530

    Update and rename modu-free-v1.0.0.28.js to modu-free-v1.0.0.29.js

--- modu-free-v1.0.0.29.js
+++ modu-free-v1.0.0.29.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
   (function(){
     if(document.getElementsByClassName(id)[0]==null)return;
   document.querySelectorAll("."+id).forEach((e)=>{
-   addOnScrollListener(e,2);
+   addOnScrollListener(e);
  });//query
 })();
  
@@ -48,7 +48,7 @@ function download_link_loader(){
   }
   download_url_btn.parentElement.style.display="block";
    }else{
-   download_cou.innerHTML='Wait <font color="yellowgreen"> '+cou+' </font> for download link';
+   download_cou.innerHTML='<br>Wait <font color="yellowgreen"> '+cou+' </font> for download link <br>';
    cou--;
    }
   },1100);
@@ -70,6 +70,15 @@ function download_link_loader(){
  
 
 
+function getTimeAfter(e) {
+  if(e.hasAttribute("timeAfter")){
+    let i=parseInt(e.getAttribute("timeAfter"));
+     if(i!=NaN){
+      return i; 
+     }
+  }
+  return 2;
+}
 
 
 
@@ -79,17 +88,16 @@ function download_link_loader(){
 
 
 
-
-function addOnScrollListener(element,timeafter) {
+function addOnScrollListener(element) {
   
-  loadAdsReact($(element),true)
+ let i=loadAdsReact($(element),true)
   
-  let cond2=true,saf=(timeafter!=undefined)?timeafter:2,cond3=saf;
+  let cond2=true,saf=getTimeAfter(element),cond3=saf;
   
   document.addEventListener("scroll",() => {
  
  let o=$(element);
- let cond=$(document).scrollTop()>(o.offset().top-600)&&$(document).scrollTop()<(o.offset().top+300);
+ let cond=$(document).scrollTop()>(o.offset().top-(i.height*2))&&$(document).scrollTop()<(o.offset().top+i.height);
  
  if(cond){
  if(cond2){
@@ -120,32 +128,54 @@ function loadAdsReact(element,b) {
   if(b)element.html("");
   element[0].style.textAlign="center";
  let i=document.createElement("iframe");
+ $(i).attr("scrolling","no");
  let st=i.style;
- st.boxShadow= "0px 0px 5px 0px #b8b4b4";
- i.width="0";
- i.height="0";
-$(i).on("load",()=>{
-   i.width="300";
-   let h=0;
-  let intv= setInterval(function() {
-   i.height=(h>250?250:h)+"";
-   if(h>250)clearInterval(intv);
-   h=h+10;
-   }, 1);
-   
- });
  st.border="none";
  st.padding="0px";
  st.margin="0px";
- st.borderRadius="4px";
- 
  element.append(i);
- 
- var doc = i.contentDocument ? i.contentDocument :(i.contentWindow ? i.contentWindow.document : i.document)
+  loadRandom([1,1,1,1,2,2,2,1,1,2,2,3,1,2],i);
+  return i;
+}
 
+
+function loadRandom(p,i) {
+ 
+ var doc = i.contentDocument ? i.contentDocument :(i.contentWindow ? i.contentWindow.document : i.document),
+ ind=(p[getRandomI(0,p.length)]),
+ text=getAdsS(ind);
+ i.height=getAdsHeight(ind);
+ i.width=getAdsWidth(ind);
+ 
+ 
 doc.open();
 
-doc.write(`<html><body style="margin:0px; padding:0px; overflow:hidden; text-align: center;"><script type="text/javascript">
+doc.write(text);
+
+doc.close();
+
+}
+
+
+function getRandomI(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getAdsHeight(i) {
+  return (i==3)?600:(i==2)?50:250;
+}
+
+
+function getAdsWidth(i) {
+   return (i==3)? 320:(i==2)?320:300;
+}
+
+
+
+
+function getAdsS(r){
+  
+  var a=`<html><body style="margin:0px; padding:0px; overflow:hidden; text-align: center;"><script type="text/javascript">
 	atOptions = {
 		'key' : '9107021bda86af1c978cb74fe0552c54',
 		'format' : 'iframe',
@@ -154,10 +184,28 @@ doc.write(`<html><body style="margin:0px; padding:0px; overflow:hidden; text-ali
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://abandonrecommendationwars.com/9107021bda86af1c978cb74fe0552c54/invoke.js"></scr' + 'ipt>')
-</script></body></html>`);
-
-doc.close();
-
+</script></body></html>`
+,b=`<script type="text/javascript">
+	atOptions = {
+		'key' : 'e5ff2ee77fad9f6f2239cac3e396bc6e',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://abandonrecommendationwars.com/e5ff2ee77fad9f6f2239cac3e396bc6e/invoke.js"></scr' + 'ipt>');
+</script>`,
+c=`<script async="async" data-cfasync="false" src="//abandonrecommendationwars.com/7bbead7bb73b15bb822cdd775fdd25db/invoke.js"></script>
+<div id="container-7bbead7bb73b15bb822cdd775fdd25db"></div>`;
+return (r==3)?c:(r==2)?b:a;
+  
 }
+
+
+
+
+
+
+
 
 })()
